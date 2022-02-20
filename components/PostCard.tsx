5863d912f1835beca650cf85622948e32af19716
JerryZhang0920 <jinxuan0920@gmail.com>
2022-02-19 21:39:59 -0500

    added file

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -46,7 +46,7 @@ const PostCard = ({ post }) => {
             </p>
             <div className="text-center">
                 <Link href={`/post/${post.slug}`}>
-                    <span className="transition duration-500 transform hover:-translate-y-1">
+                    <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
                         Continue Reading
                     </span>
                 </Link>
