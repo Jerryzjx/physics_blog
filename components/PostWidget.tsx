d2d78e022844923cfb7c5049998ef88701209690
JerryZhang0920 <jinxuan0920@gmail.com>
2022-02-19 22:34:14 -0500

    added file

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -17,9 +17,16 @@ const PostWidget = ({ categories, slug }) => {
         } , [slug])
     return(
         <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
-            <h3>
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
                 {slug ? 'Related Posts' : "Recent Posts"}
             </h3>
+            {relatedPosts.map((post) => (
+                <div key={post.title} className="flex items-center w-full mb-4">
+                   <div className = >
+
+                   </div>
+                </div>
+                ))}
         </div>
     )
 }
