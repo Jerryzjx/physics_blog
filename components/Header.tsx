b5310dc58292ac061c040a0eba26971c57635dd2
JerryZhang0920 <jinxuan0920@gmail.com>
2022-02-20 14:35:45 -0500

    fixed typo

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
 
     return (
         <div className="container mx-auto px-10 mb-8">
-            <div className="border-b w-full inline-block border-blue-400 py-8">
+            <div className="border-b w-full inline-block border-white py-8">
                 <div className="md:float-left block">
                     <Link href="/">
                         <span className="cursor-pointer font-bold text-4xl text-white">Jerry's Blog</span>
